654ca101058cdf7038d7448ed6996bc0f402d51d
Roman Rader <roman.rader@grammarly.com>
2024-01-29 09:59:12 +0200

    lesson start

---
+++ static/lessons/Початок уроку.bat
@@ -0,0 +1,4 @@
+@echo off
+setlocal enabledelayedexpansion
+set "computerName=%COMPUTERNAME%"
+start chrome "https://docs.google.com/forms/d/e/1FAIpQLSfQ_H8QByJHY_m92aymCsHdxeouo3SkmEnNrf_sQrLwYr0VnQ/viewform?usp=pp_url&entry.1218425355=%computerName%"
